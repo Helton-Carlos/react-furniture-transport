94d799bb58d2b081469bd768abbc3e6724b94da0
Helton Brito <heltoncarlospa@hotmail.com>
2025-06-29 11:25:41 -0300

    feat: add daysy ui

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div>
-        <h1 className="bg-amber-400 text-3xl font-bold underline">
+        <h1 className="bg-accent p-4 text-3xl font-bold">
           {name}
         </h1>
       </div>
