9f993470ea71edeb96d67270d0f267ca7ba96da7
Helton Brito <heltoncarlospa@hotmail.com>
2025-07-20 16:49:12 -0300

    feat: create btn go back

---
+++ src/components/goBack.tsx
@@ -0,0 +1,20 @@
+
+import { FaArrowLeft } from "react-icons/fa";
+
+export default function GoBack() {
+  function handleGoBack() {
+    window.history.back();
+  }
+
+  return (
+    <button
+      onClick={handleGoBack}
+      type="button"
+      aria-label="Voltar"
+      className="flex items-center m-8 gap-2 text-lg font-bold text-gray-700 cursor-pointer hover:text-gray-300"
+    >
+      <FaArrowLeft />
+      Voltar
+    </button>
+  );
+}
--- src/pages/TypeCard.tsx
+++ src/pages/TypeCard.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import GoBack from "../components/goBack";
 
 function TypeCard() {
   const [cards] = useState([
@@ -27,29 +28,33 @@ function TypeCard() {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-4 py-8 bg-gray-50">
-      <p className="bg-accent px-4 py-2 text-3xl font-bold my-8 rounded">Tipos de Envio</p>
+    <>
+      <GoBack />
 
-      <div className="flex flex-wrap justify-center gap-8">
-        {cards.map(({ title, description, image, type }, i) => (
-          <div key={i} className="card bg-base-100 image-full w-96 shadow-sm">
-            <figure>
-              <img src={image} alt={title} className="object-cover h-56 w-full" />
-            </figure>
-            <div className="card-body">
-              <h2 className="card-title">{title}</h2>
-              <p>{description}</p>
-              <div className="card-actions justify-end">
-                <button className="btn-primary" onClick={() => escolherTipo(type)}>
-                  Avançar
-                </button>
+      <div className="flex flex-col items-center justify-center min-h-screen px-4 py-8">
+        <p className="bg-accent px-4 py-2 text-3xl font-bold my-8 rounded">Tipos de Envio</p>
+
+        <div className="flex flex-wrap justify-center gap-8">
+          {cards.map(({ title, description, image, type }, i) => (
+            <div key={i} className="card bg-base-100 image-full w-96 shadow-sm">
+              <figure>
+                <img src={image} alt={title} className="object-cover h-56 w-full" />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title">{title}</h2>
+                <p>{description}</p>
+                <div className="card-actions justify-end">
+                  <button className="btn-primary" onClick={() => escolherTipo(type)}>
+                    Avançar
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
 
-    </div>
+      </div>
+    </>
   );
 }
 
