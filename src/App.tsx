f335c9e3c8606d6772e3022ac45fa20ebe9538e9
Helton Brito <heltoncarlospa@hotmail.com>
2025-07-21 20:58:18 -0300

    fix: remove page contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Contact from './pages/Contact';
 import Calendar from './pages/Calendar';
 import TypeCard from './pages/TypeCard';
+import CEP from './pages/CEP'
 
 export default function App() {
   return (
@@ -9,7 +9,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Calendar />} />
         <Route path="/tipo-envio" element={<TypeCard />} />
-        <Route path="*" element={<Contact />} />
+        <Route path="/cep" element={<CEP />} />
       </Routes>
     </Router>
   );
