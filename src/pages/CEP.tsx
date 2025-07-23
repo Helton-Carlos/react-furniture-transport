1b75e0026ff7e1eefd2509ea3f839095fe738aab
Helton Brito <heltoncarlospa@hotmail.com>
2025-07-23 19:56:44 -0300

    feat: add alias in components

--- src/pages/CEP.tsx
+++ src/pages/CEP.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import GoBack from "../components/goBack";
-import { maskCep } from "../utils/maskCEP";
+import GoBack from "@components/goBack";
+import { maskCep } from "@utils/maskCEP";
 import { FaMapMarkerAlt, FaFlagCheckered } from 'react-icons/fa';
 
 function CEP() {
--- src/pages/TypeCard.tsx
+++ src/pages/TypeCard.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import GoBack from "../components/goBack";
+import GoBack from "@components/goBack";
 
 function TypeCard() {
   const navigate = useNavigate();
